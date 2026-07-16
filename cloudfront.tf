f1b1d274c9f362d3afcc5607423fd65d8314ad84
Yashasvi SJ <yashasvisj2000@gmail.com>
2026-07-17 01:01:14 +0530

    Updated CloudFront configuration

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,53 +1,46 @@
+
+resource "aws_cloudfront_origin_access_control" "frontend_oac" {
+  name                              = "frontend-oac"
+  description                       = "OAC for frontend S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 resource "aws_cloudfront_distribution" "website" {
 
-  enabled = true
+  enabled             = true
+  default_root_object = "index.html"
 
   origin {
-
-    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
-
-    origin_id = "frontend"
-
+    domain_name              = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id                = "frontend"
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend_oac.id
   }
 
+
+
   default_cache_behavior {
-
-    allowed_methods = ["GET", "HEAD"]
-
-    cached_methods = ["GET", "HEAD"]
-
-    target_origin_id = "frontend"
-
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "frontend"
     viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
-
       query_string = false
 
       cookies {
-
         forward = "none"
-
       }
-
     }
-
   }
 
   restrictions {
-
     geo_restriction {
-
       restriction_type = "none"
-
     }
-
   }
 
   viewer_certificate {
-
     cloudfront_default_certificate = true
-
   }
-
 }
--- s3.tf
+++ s3.tf
@@ -14,3 +14,37 @@ resource "aws_s3_object" "index" {
   content_type = "text/html"
 
 }
+data "aws_iam_policy_document" "frontend_policy" {
+
+  statement {
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.frontend_bucket.arn}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        aws_cloudfront_distribution.website.arn
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = data.aws_iam_policy_document.frontend_policy.json
+}
